4084ae25f2ad16c8889dfc3f6085e451c7186b2d
nirdesh17 <nirdeshdevadiya17@gmail.com>
2023-04-17 11:38:56 +0530

    Splash Screen Added

--- app/src/main/java/com/example/projectlathidataapp/MainActivity.kt
+++ app/src/main/java/com/example/projectlathidataapp/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.projectlathidataapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.projectlathidataapp.ui.theme.ProjectLathiDataAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +28,37 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    SplashContent()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
+//preview function
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ProjectLathiDataAppTheme {
-        Greeting("Android")
+        SplashContent()
     }
 }
+@Composable
+fun SplashContent() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        val context = LocalContext.current// we can't use context directly in composable function hence we need this
+        Button(onClick = { Toast.makeText(context, "login", Toast.LENGTH_SHORT).show() }) {
+            Text(text = "Login")
+        }
+        Spacer(Modifier.height(16.dp))// space between buttons
+
+        Button(onClick = { Toast.makeText(context, "Register", Toast.LENGTH_LONG).show() }) {
+            Text(text = "Register")
+        }
+    }
+}
+
--- app/src/main/java/com/example/projectlathidataapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/projectlathidataapp/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.example.projectlathidataapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val PaleGreen = Color(0xFF98FB98)
+val PaleGreen1 = Color(0xFF58F858)
+val PaleGreen2 = Color(0xFFA4FBA4)
+//val Teal200 = Color(0xFF03DAC5)
